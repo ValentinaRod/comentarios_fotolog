0705df2c1ed1cf1b43e9da381382a5d430e4b182
ValentinaRod <v.rodriguez1425@gmail.com>
2017-06-18 23:51:34 -0400

    js 3 parte

--- js/main.js
+++ js/main.js
@@ -33,3 +33,14 @@ function guardarDatos(){
 	var nombre = document.createTextNode(kei);/*crea un nodo hijo*/
 	titulo.classList.add("datos-usuario");/*agrega una clase al padre*/
 	titulo.appendChild(nombre);/*le asigna un padre al nodo hijo*/
+
+	var parrafo = document.createElement("p");/*titulo es el padre*/
+	var comentario = document.createElement("values");/*nombre es el hijo*/
+	parrafo.classList.add("post");/*agrega una clase al padre*/
+	parrafo.appendChild(comentario);/*le asigna un padre al nodo hijo*/
+
+/*Luego asignamos el padre(caja) a los hijos (nombre y comentarios) para guardarlos.*/
+	caja.appendChild(nombre);
+	caja.appendChild(comentario);
+/*finalmente le asignamos el padre (contenedor) al hijo (caja), donde me muestra todo el contenedor creado*/
+	contenedor.appendChild(caja);
