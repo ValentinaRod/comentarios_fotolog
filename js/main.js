d6ca82c5fe8aa74fafb76200d2601895e3fe4e16
ValentinaRod <v.rodriguez1425@gmail.com>
2017-06-18 23:49:47 -0400

    añado js 1 parte

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 /*aquí va tu código*/
+/*aquí va tu código*/
+/*key es donde se encuentra almacenada la clave ej: nombres*/
+/*funcion boton*/
+function init(){
+	var button = document.getElementById("boton");
+	button.addEventListener("click", guardarDatos);
+	guardarDatos();
+}
+
+/*funcion guardar datos*/
+function guardarDatos(){
+	var clave = document.getElementById("clave").value;
+	var valor = document.getElementById("valor").value;
+
+	localStorage.setItem(clave, valor);
+
+	/*limpia contenedor para no repetir valores en html*/
+	var contenedor = document.getElementById("ale");
+	contenedor.innerHTML="";
